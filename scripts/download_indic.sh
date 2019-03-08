0a539921e289bb3a1262bae464270812c5ddf148
Francisco Guzman <guzmanhe@gmail.com>
2019-03-07 21:53:41 -0800

    Reverting to the previous version of indic nlp

--- scripts/download_indic.sh
+++ scripts/download_indic.sh
@@ -12,6 +12,9 @@ INDICNLP=$ROOT/indic_nlp_library
 if [ ! -e $INDICNLP ]; then
     echo "Cloning Indic NLP Library..."
     git -C $ROOT clone https://github.com/anoopkunchukuttan/indic_nlp_library.git
+    pushd $INDICNLP
+    git reset --hard 0a5e01f2701e0df5bc1f9905334cd7916d874c16
+    popd
 else
     echo "Indic is already pulled from github. Skipping."
 fi
