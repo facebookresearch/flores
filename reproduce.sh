e6087820f5a870710c8988793f8aa7fc70e7161c
Peng-Jen Chen <pipibjc@devfair0209.h2.fair>
2019-10-29 10:50:05 -0700

    Add si-en bt model configs

--- reproduce.sh
+++ reproduce.sh
@@ -7,7 +7,7 @@
 #!/bin/bash
 # Script to reproduce iterative back-translation baseline
 
-SUPPORT_LANG_PAIRS="ne_en|en_ne"
+SUPPORT_LANG_PAIRS="ne_en|en_ne|si_en|en_si"
 
 if [[ $# -lt 1 ]] || ! [[ "$1" =~ ^($SUPPORT_LANG_PAIRS)$ ]]; then
   echo "Usage: $0 LANGUAGE_PAIR($SUPPORT_LANG_PAIRS)"
@@ -20,4 +20,8 @@ if [[ $LANG_PAIR = "ne_en" ]]; then
   python scripts/train.py --config configs/neen.json --databin $PWD/data-bin/wiki_ne_en_bpe5000/
 elif [[ $LANG_PAIR = "en_ne" ]]; then
   python scripts/train.py --config configs/enne.json --databin $PWD/data-bin/wiki_ne_en_bpe5000/
+elif [[ $LANG_PAIR = "si_en" ]]; then
+  python scripts/train.py --config configs/sien.json --databin $PWD/data-bin/wiki_si_en_bpe5000/
+elif [[ $LANG_PAIR = "en_si" ]]; then
+  python scripts/train.py --config configs/ensi.json --databin $PWD/data-bin/wiki_si_en_bpe5000/
 fi
